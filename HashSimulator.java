43df90bbb5a2c73af78d54b8d2e9c6ac492823e8
Gathrean Dela Cruz <gathrean@icloud.com>
2023-10-30 09:54:42 -0700

    Implemented code in H1 to work with "BENNY" input

--- HashSimulator.java
+++ HashSimulator.java
@@ -1,8 +1,33 @@
 // Name: Gathrean Dela Cruz
 // ID: A01167248
+// Set: 3G
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * HashSimulator that will process an input list three times.
+ * One for each of three different hash functions.
+ */
 public class HashSimulator {
 
+    /**
+     * Alphabet hash map
+     */
+    private HashMap<Character, Integer> alphabetMap;
+
+    /**
+     * Alphabet hash map constructor
+     */
+    public HashSimulator() {
+        alphabetMap = new HashMap<>();
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabetMap.put(alphabet[i], i + 1);
+        }
+    }
 
     public int[] runHashSimulation(String[] hashKeys, int tableSize) {
 
@@ -27,6 +52,37 @@ public class HashSimulator {
      * @return The hash value of 60 mod HT size
      */
     public int H1(String hashKey, int tableSize) {
+
+        // Create a new, empty hash table of the given size
+        //      (hash table is also an array of Strings)
+        HashMap<Integer, String> h1HashTable = new HashMap<>(tableSize);
+
+        int collisionCount = 0; // Set collision count to 0
+        int probeCount = 0; // Set probe count to 0
+
+        // Calculate hash value using H1 function and take modulo 60
+        for (char c : hashKey.toUpperCase().toCharArray()) {
+            int charValue = alphabetMap.get(c);
+            int hashValue = (charValue + probeCount) % 60;
+
+            // Calculate the hash value modulo tableSize (HT size)
+            hashValue = hashValue % tableSize;
+
+            // Handle collisions with linear probing
+            while (h1HashTable.containsKey(hashValue)) {
+                collisionCount++;
+                probeCount++;
+                hashValue = (hashValue + 1) % tableSize;
+            }
+
+            // Store key in hash table
+            h1HashTable.put(hashValue, String.valueOf(c));
+            probeCount++;
+        }
+
+        // Print collision and probe counts for testing purposes
+        System.out.println("Collision Count: " + collisionCount);
+        System.out.println("Probe Count: " + probeCount);
 
         return tableSize;
     }
@@ -78,5 +134,10 @@ public class HashSimulator {
      */
     public static void main(String[] args) {
         // Testing purposes, won't be marked.
+        HashSimulator hashTable = new HashSimulator();
+        int tableSize = 10; // Set your desired table size
+        String input = "BENNY"; // Input string
+        int result = hashTable.H1(input, tableSize);
+        System.out.println("Result: " + result);
     }
 }
